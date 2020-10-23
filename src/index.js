d25b090dac350214db7ca43aa513c57a56d2911f
Nikita Fedarenchyk <fnikita01@mail.ru>
2020-10-23 15:54:07 +0300

    feat: add some code to pass the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+  let buff = [];
+  let calc = 0;
+  let res = '';
+  let currNum = Math.abs(n);
+  do{
+      let num = Math.floor(currNum / 10);
+      let diff = currNum - num * 10;
+      buff[calc++] = diff;
+      currNum = num;
+  } while (currNum > 0);
+    for (let i = 0; i < buff.length; i++) {
+        res += buff[i];
+    }
+  return parseInt(res);
 }
